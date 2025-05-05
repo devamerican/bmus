31d8ba904ae3113e46755124807e487cbe424115
ajay <ajaypathak2527@gmail.com>
2025-05-05 09:18:34 +0530

    update about page

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -88,16 +88,17 @@ const urlFor = (source: SanityImageSource) =>
     : null;
 
   return (
-    <section className="max-w-5xl mx-auto px-4 my-10 lg:my-16 lg:pb-16" >
+    <section className="max-w-7xl mx-auto px-4 my-10 lg:my-16 lg:pb-16" >
       <h1 className="text-h1 text-center mb-20" >{aboutPageData.intro.heading}</h1> 
       <h2 className="text-h3 mb-8">{aboutPageData.intro.subheading}</h2>
-      <div className="space-y-8" >
-        {aboutPageData?.intro?.content && <PortableText value={aboutPageData?.intro?.content} />}
-        {/* <p>Education Abroad Services Overseas CAREER Consultants provides “One Stop Solution for All Your International Education Needs”.</p>
-        <p>We offer a wide-ranging portfolio of outstanding and brilliantly managed services right from pre-admission to post landing services designed to suit the individual needs of the students.Our quality counseling distinguishes us from others in a way that we offer personalized counseling where there is direct involvement of the directors who have been educated in the finest institutes in India and abroad and have first-hand experience of the international culture and education system.</p>
-        <p>Our team put forward sincere efforts and commitment on each student application case to help student realize the dream of studying in the best universities of the world. We are committed to contribute to our country and world in the education arena. Excellence is the top focus for our company. Our company continuously improves our services to deliver the best services and optimum satisfaction to all.</p>
-        <p>We aim at bringing education in every corner of the country. Every town, village, district should be flooded with education is the motto that we believe in. With its corporate office in FARIDABAD NCR, EDUCATION ABROAD SERVICES is spreading its spectrum by getting functional PAN India to cater to maximum.</p> */}
-      </div>    
+      {/* <div className="flex flex-col lg:flex-row gap-6 items-center *:basis-1/2" > 
+        <div>
+          <Image src={urlFor(aboutPageData?.intro?.introImage)?.url() ?? ""} width={1920} height={1080} alt="intro image"  />
+        </div>   
+      </div> */}
+        <div className="prose !max-w-full" >
+          {aboutPageData?.intro?.content && <PortableText value={aboutPageData?.intro?.content} />}
+        </div> 
 
 
         {/* Achievements */}
@@ -107,7 +108,7 @@ const urlFor = (source: SanityImageSource) =>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 place-content-center place-items-center gap-4 lg:gap-6" >
             {
                 aboutPageData.achievements.achievementItems?.map((item: { icon: IconName, title: string, _key: string }) => (
-                        <div key={item._key} className="hover:shadow transition-shadow p-6 rounded-lg bg-muted w-full" >
+                        <div key={item._key} className="hover:shadow transition-shadow p-6 rounded-lg bg-muted w-full min-h-40" >
                             <div>
                                 <DynamicIcon name={item.icon as IconName} size={32} className=" mb-6 text-blue-500" />
                                 <h4 className="text-lg font-semibold " >{item.title}</h4>
@@ -126,7 +127,7 @@ const urlFor = (source: SanityImageSource) =>
         <h2 className="text-h2 mb-1" >{aboutPageData?.ourTeam?.heading}</h2>
         <p className="text-muted-foreground mb-12" >{aboutPageData?.ourTeam?.subHeading}</p>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 lg:gap-6" >
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 2xl:grid-cols-5 gap-4 lg:gap-6" >
         {
             aboutPageData?.ourTeam?.teamMembers?.map((item: any) => (
                 <div key={item._key} className="flex flex-col items-center justify-center">
